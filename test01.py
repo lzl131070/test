1916b8f7fdc9fc47d586cc2527373df352bb702a
lzl131070 <616108722@qq.com>
2019-01-07 20:53:36 +0800

    add numpy way

--- test01.py
+++ test01.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 #
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -6,15 +7,22 @@ class Num(object):
         self.list=list
         self.num=num
     def fn(self):
+        res = []
         for i in self.list:
-            for j in self.list:
-                if i!=j and i+j==self.num:
-                    return self.list.index(i),self.list.index(j)
 
+            for j in self.list[self.list.index(i)+1:]:
+                if i+j==self.num:
+                    res.append([self.list.index(i),self.list.index(j)])
+        return res
 
-def main():
-    a=Num([1,2,3,4,5,7,8,9],10)
-    print(a.fn())
 
 if __name__ == '__main__':
-    main()
+    a = Num([1, 2, 3, 4, 5, 7, 8, 9], 10)
+    print(a.fn())
+
+    # numpy way
+    n1 = np.array([1, 2, 3, 4, 5, 7, 8, 9])
+    n2 = n1.reshape((-1,1))
+    n3 = n1 + n2
+    l = np.argwhere(n3==10)
+    print(l[l[:,0]<l[:,1]])
